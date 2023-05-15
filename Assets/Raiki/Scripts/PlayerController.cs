95b53da433bff11e3d9b68b279cf7518c7d380ca
2PC-19\student <20cge0007kr@edu.sendai-com.ac.jp>
2023-05-15 10:43:13 +0900

    プレイヤーの設定できました。微調整で完成だと思われます

---
+++ Assets/Raiki/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 3.0f;
+    public Vector2 rotationSpeed = new Vector2(0.1f,0.1f);
+
+    private Vector2 lastMousePosition;
+    private Vector2 newAnle = Vector2.zero;
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.forward * moveSpeed * Time.deltaTime;
+        }
+        if(Input.GetKey(KeyCode.S))
+        {
+            transform.position -= transform.forward * moveSpeed * Time.deltaTime;
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            transform.position += transform.right * moveSpeed * Time.deltaTime;
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.position -= transform.right * moveSpeed * Time.deltaTime;
+        }
+
+
+    }
+}
