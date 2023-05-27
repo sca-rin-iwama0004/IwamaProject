c69a0355fd2e54381645b8488e96f3b3f3ddbcbc
PG22-01 <>
2023-05-27 16:59:01 +0900

    峰岸君の仕様に合わせての修正になります。

--- Assets/Raiki/Scripts/PlayerController.cs
+++ Assets/Raiki/Scripts/PlayerController.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -10,25 +12,33 @@ public class PlayerController : MonoBehaviour
     private Vector2 lastMousePosition;
     private Vector2 newAnle = Vector2.zero;
 
+    GameManager GM;
+
+    private void Start()
+    {
+        GM = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+
     void Update()
     {
-        if(Input.GetKey(KeyCode.W))
-        {
-            transform.position += transform.forward * moveSpeed * Time.deltaTime;
+        if(GM.PlayMode == GameManager.Mode.Play) { 
+            if(Input.GetKey(KeyCode.W))
+            {
+                transform.position += transform.forward * moveSpeed * Time.deltaTime;
+            }
+            if(Input.GetKey(KeyCode.S))
+            {
+                transform.position -= transform.forward * moveSpeed * Time.deltaTime;
+            }
+            if(Input.GetKey(KeyCode.D))
+            {
+                transform.position += transform.right * moveSpeed * Time.deltaTime;
+            }
+            if(Input.GetKey(KeyCode.A))
+            {
+                transform.position -= transform.right * moveSpeed * Time.deltaTime;
+            }
         }
-        if(Input.GetKey(KeyCode.S))
-        {
-            transform.position -= transform.forward * moveSpeed * Time.deltaTime;
-        }
-        if(Input.GetKey(KeyCode.D))
-        {
-            transform.position += transform.right * moveSpeed * Time.deltaTime;
-        }
-        if(Input.GetKey(KeyCode.A))
-        {
-            transform.position -= transform.right * moveSpeed * Time.deltaTime;
-        }
-
 
     }
 }
