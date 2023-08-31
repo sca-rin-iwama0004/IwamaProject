2a29894216ba547f975b8eabc6ad5d978408803f
minegishi <>
2023-08-31 15:16:02 +0900

    肉・棒のテキスト追加

---
+++ Assets/minegishi/Scripts/Meat.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Meat : MonoBehaviour
+{
+
+    GameManager GM;
+    TextWriter text;
+
+    public bool meatText = false;
+
+    void Start()
+    {
+        GM = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+
+
+    void Update()
+    {
+        if (GM.PlayMode == GameManager.Mode.Play)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit))
+                {
+                    if (hit.collider.gameObject.name == "Meat")
+                    {
+                        GameManager.Instance.StickGet = true;
+                        meatText = true;
+                        GM.PlayMode = GameManager.Mode.Text;
+                        TextWriter text = GameObject.Find("Text").GetComponent<TextWriter>();
+                        StartCoroutine(text.Cotest());
+                    }
+                    //Destroy(clickedGameObject);
+
+                }
+                //Debug.Log(clickedGameObject);
+            }
+        }
+    }
+}
---
+++ Assets/minegishi/Scripts/Stick.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stick : MonoBehaviour
+{
+    GameManager GM;
+    TextWriter text;
+
+    public bool stickText = false;
+
+    void Start()
+    {
+        GM = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+
+
+    void Update()
+    {
+        if (GM.PlayMode == GameManager.Mode.Play)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit))
+                {
+                    if (hit.collider.gameObject.name == "Stick")
+                    {
+                        GameManager.Instance.StickGet = true;
+                        stickText = true;
+                        GM.PlayMode = GameManager.Mode.Text;
+                        TextWriter text = GameObject.Find("Text").GetComponent<TextWriter>();
+                        StartCoroutine(text.Cotest());
+                    }
+                    //Destroy(clickedGameObject);
+
+                }
+                //Debug.Log(clickedGameObject);
+            }
+        }
+    }
+}
